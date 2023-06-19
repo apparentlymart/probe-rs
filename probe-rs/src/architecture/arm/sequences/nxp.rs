ef754185e448b32616df69803b71da683997c680
Martin Atkins <mart@degeneration.co.uk>
2023-06-18 17:15:04 -0700

    Support for NXP i.MX RT5xxS devices
    The target YAML was initially generated using target-gen using CMSIS packs,
    but then slightly modified as follows:
     - Manually added the FlexSPI0 memory-mapped space as non-volatile memory
       so that probe-rs understands it as program space. The flash algorithm
       for it is unmodified from the CMSIS packs, and is apparently written
       for the official RT595S evaluation board in particular. I don't know if
       it's general enough to work with flash chips beyond the one included
       on the evaluation board. (The main algorithm implementation actually
       lives in the boot ROM; the provided algorithm just jumps into it.)
     - Additional stack space for the flash algorithm, since the ROM software
       was apparently overflowing the default stack.
     - Clearer names for the memory regions, matching how they are described
       in the reference manual.
     - Start and end addresses padded to 8 hex characters so they are all
       consistent and easier to compare with the memory map in the reference
       manual.
     - Core names consistently set to "cm33". The original generated YAML was
       inconsistent, with RT595S using a mixture of "cm33" and "main" that
       caused the resulting memory map to malfunction. (RT595S has a second
       processor DSP core that is also debuggable, so "cm33" is to distinguish
       the Cortex-M33 core from the DSP core, although there's no debug
       support for the DSP core here yet -- it's an Xtensa-based core.)
     - Only one "variant" per chip type. The pack defines two each but they
       have identical memory maps.
    
    These chips have no on-board flash memory and so instead they boot into
    an immutable boot ROM which then probes various other peripherals to see
    if they can provide application code. Debug is disabled while the boot
    ROM is running, and so a normal reset catch is non-functional on this
    chip, and there's no fixed entry point to attach a breakpoint to.
    
    Instead, the reference manual documents an address that gets read once the
    boot process is complete, and recommends using a data watchpoint to catch
    that read and halt the processor.
    
    Furthermore, if the boot ROM cannot find a valid program image in any of
    the places it looks the chip enters its ISP mode, in which debug remains
    disabled. In that case the only way to achieve debug control of the chip
    is to interact with a custom "debug mailbox" interface that can command
    the ISP code to yield control to a busy loop with debug enabled, after
    which we can finally halt the core.
    
    The custom debug sequence implementation handles all of these oddities to
    still achieve a reset-and-halt as long as the chip hasn't been configured
    for Debug Authentication (reference manual section 60.6), which isn't
    supported here at all. The sequence implementation is a direct port of
    the code in the CMSIS-Pack for MIMXRT595S.

--- probe-rs/src/architecture/arm/sequences/nxp.rs
+++ probe-rs/src/architecture/arm/sequences/nxp.rs
@@ -10,10 +10,11 @@ use crate::{
     architecture::arm::{
         ap::{AccessPort, AccessPortError, ApAccess, GenericAp, MemoryAp, DRW, IDR, TAR},
         communication_interface::{FlushableArmAccess, Initialized},
+        component::TraceSink,
         core::armv7m::{Aircr, Demcr, Dhcsr},
         dp::{Abort, Ctrl, DpAccess, Select, DPIDR},
-        memory::adi_v5_memory_interface::ArmProbe,
-        ApAddress, ArmCommunicationInterface, ArmError, DapAccess, DpAddress,
+        memory::{adi_v5_memory_interface::ArmProbe, CoresightComponent},
+        ApAddress, ArmCommunicationInterface, ArmError, ArmProbeInterface, DapAccess, DpAddress,
     },
     core::MemoryMappedRegister,
 };
@@ -568,5 +569,251 @@ impl ArmDebugSequence for MIMXRT11xx {
 
         interface.read_word_32(Dhcsr::get_mmio_address())?;
         Ok(())
+    }
+}
+
+/// Debug sequences for MIMXRT5xxS MCUs.
+///
+/// MCUs in this series do not have any on-board flash memory, and instead
+/// there is an unprogrammable boot ROM which attempts to find a suitable
+/// program from a variety of different sources. The entry point for
+/// application code therefore varies depending on the boot medium.
+///
+/// Because the system begins execution in the boot ROM, it isn't possible
+/// to use a standard reset vector catch on this platform. Instead, the series
+/// datasheet (section 60.3.4) describes the following protocol:
+///
+/// - Set a data watchpoint for a read from location 0x50002034.
+/// - Use SYSRESETREQ to reset the core and peripherals.
+/// - Wait 100ms to allow the boot ROM to re-enable debug.
+/// - Check whether the core is halted due to the watchpoint, by checking DHCSR.
+/// - If the core doesn't halt or halts for some reason other than the
+///   watchpoint, use the special debug mailbox protocol to exit the ISP mode
+///   and enter an infinite loop, at which point we can halt the MCU explicitly.
+/// - Clear the data watchpoint.
+///
+/// The debug mailbox protocol handles, among other things, recovering debug
+/// access when the part enters its ISP mode. ISP mode has debug disabled to
+/// prevent tampering with the system's security features. Datasheet
+/// section 60.3.1 describes the special debug recovery process.
+//
+// This type's [`ArmDebugSequence`] implementation is based on the sequences
+// defined in the CMSIS Pack for MIMXRT595S, but should be compatible with
+// all parts in this series. The implementation closely follows the CMSIS Pack
+// structure and its comments for ease of comparison.
+pub struct MIMXRT5xxS {}
+
+impl MIMXRT5xxS {
+    const DWT_COMP0: u64 = 0xE0001020;
+    const DWT_FUNCTION0: u64 = 0xE0001028;
+    const SYSTEM_STICK_CALIB_ADDR: u32 = 0x50002034;
+
+    /// Create a sequence handle for the MIMXRT5xxS.
+    pub fn create() -> Arc<dyn ArmDebugSequence> {
+        Arc::new(Self {})
+    }
+
+    /// Runtime validation of core type.
+    fn check_core_type(&self, core_type: crate::CoreType) -> Result<(), ArmError> {
+        if core_type != crate::CoreType::Armv8m {
+            // Caller has selected the wrong chip name, presumably.
+            return Err(ArmError::ArchitectureRequired(&["ARMv8"]));
+        }
+        Ok(())
+    }
+
+    /// A port of the "WaitForStopAfterReset" sequence from the CMSIS Pack for
+    /// this chip.
+    fn wait_for_stop_after_reset(&self, probe: &mut dyn ArmProbe) -> Result<(), ArmError> {
+        tracing::debug!("waiting for MIMXRT5xxS halt after reset");
+
+        // Note: despite the name of this sequence in the CMSIS Pack, the
+        // given implementation doesn't actually _wait_ for stop, and instead
+        // just forces stopping itself. This is because there is no reliable
+        // way to detect reset that works in all cases: the boot ROM might
+        // jump into ISP mode, in which case we need to use the debug mailbox
+        // to regain debug control.
+
+        // Give bootloader time to do what it needs to do
+        std::thread::sleep(Duration::from_micros(100_000));
+
+        self.enable_debug_mailbox(probe.get_arm_communication_interface()?)?;
+
+        // Halt the core in case it didn't stop at a breakpiont.
+        tracing::debug!("halting MIMXRT5xxS Cortex-M33 core");
+        let mut dhcsr = Dhcsr(0);
+        dhcsr.set_c_halt(true);
+        dhcsr.set_c_debugen(true);
+        dhcsr.enable_write();
+        probe.write_word_32(Dhcsr::get_mmio_address(), dhcsr.into())?;
+        probe.flush()?;
+
+        // Clear watch point
+        probe.write_word_32(Self::DWT_COMP0, 0x0)?;
+        probe.write_word_32(Self::DWT_FUNCTION0, 0x0)?;
+        probe.flush()?;
+        tracing::debug!("cleared data watchpoint for MIMXRT5xxS reset");
+
+        Ok(())
+    }
+
+    /// A port of the "EnableDebugMailbox" sequence from the CMSIS Pack for
+    /// this chip.
+    fn enable_debug_mailbox(
+        &self,
+        interface: &mut ArmCommunicationInterface<Initialized>,
+    ) -> Result<(), ArmError> {
+        // Check AHB-AP CSW DbgStatus to decide if need enable DebugMailbox
+        let ap_addr = ApAddress {
+            dp: DpAddress::Default,
+            ap: 0,
+        };
+        if (interface.read_raw_ap_register(ap_addr, 0x0)? & 0x40) == 0 {
+            tracing::debug!("don't need to enable MIMXRT5xxS DebugMailbox");
+            return Ok(());
+        }
+
+        tracing::debug!("enabling MIMXRT5xxS DebugMailbox");
+
+        // For the rest of this work we'll use the debugger mailbox registers.
+        let ap_addr = ApAddress {
+            dp: DpAddress::Default,
+            ap: 2,
+        };
+
+        // CMSIS Pack implementation reads APIDR and DPIDR and passes each
+        // to the "Message" function, but otherwise does nothing with those
+        // results, so we skip that here.
+
+        // Active DebugMailbox
+        interface.write_raw_ap_register(ap_addr, 0x0, 0x00000021)?;
+        std::thread::sleep(Duration::from_micros(30_000));
+        interface.read_raw_ap_register(ap_addr, 0x0)?;
+
+        // Enter Debug Session
+        interface.write_raw_ap_register(ap_addr, 0x4, 0x00000007)?;
+        std::thread::sleep(Duration::from_micros(30_000));
+        interface.read_raw_ap_register(ap_addr, 0x0)?;
+
+        tracing::debug!("entered MIMXRT5xxS debug session");
+
+        Ok(())
+    }
+}
+
+impl ArmDebugSequence for MIMXRT5xxS {
+    fn debug_port_start(
+        &self,
+        interface: &mut ArmCommunicationInterface<Initialized>,
+        dp: DpAddress,
+    ) -> Result<(), ArmError> {
+        const SW_DP_ABORT: u8 = 0x0;
+        const DP_CTRL_STAT: u8 = 0x4;
+        const DP_SELECT: u8 = 0x8;
+
+        tracing::debug!("MIMXRT5xxS debug port start");
+
+        // Switch to DP Register Bank 0
+        interface.write_raw_dp_register(dp, DP_SELECT, 0x00000000)?;
+
+        // Read DP CTRL/STAT Register and check if CSYSPWRUPACK and CDBGPWRUPACK bits are set
+        let powered_down =
+            (interface.read_raw_dp_register(dp, DP_CTRL_STAT)? & 0xA0000000) != 0xA0000000;
+        if powered_down {
+            // Request Debug/System Power-Up
+            interface.write_raw_dp_register(dp, DP_CTRL_STAT, 0x50000000)?;
+
+            // Wait for Power-Up Request to be acknowledged
+            let start = Instant::now();
+            while (interface.read_raw_dp_register(dp, DP_CTRL_STAT)? & 0xA0000000) != 0xA0000000 {
+                if start.elapsed() >= Duration::from_micros(1_000_000) {
+                    return Err(ArmError::Timeout);
+                }
+            }
+
+            // SWD Specific Part of sequence
+            // TODO: Should we skip this if we're not using SWD? How?
+            // CMSIS Pack code uses: <control if="(__protocol &amp; 0xFFFF) == 2">
+            {
+                // Init AP Transfer Mode, Transaction Counter, and Lane Mask (Normal Transfer Mode, Include all Byte Lanes)
+                interface.write_raw_dp_register(dp, DP_CTRL_STAT, 0x50000F00)?;
+
+                // Clear WDATAERR, STICKYORUN, STICKYCMP, and STICKYERR bits of CTRL/STAT Register by write to ABORT register
+                interface.write_raw_dp_register(dp, SW_DP_ABORT, 0x0000001E)?;
+
+                self.enable_debug_mailbox(interface)?;
+            }
+        }
+
+        Ok(())
+    }
+
+    fn reset_system(
+        &self,
+        probe: &mut dyn ArmProbe,
+        core_type: probe_rs_target::CoreType,
+        _debug_base: Option<u64>,
+    ) -> Result<(), ArmError> {
+        self.check_core_type(core_type)?;
+
+        tracing::debug!("MIMXRT5xxS reset system");
+
+        // Halt the core
+        let mut dhcsr = Dhcsr(0);
+        dhcsr.set_c_halt(true);
+        dhcsr.set_c_debugen(true);
+        dhcsr.enable_write();
+        probe.write_word_32(Dhcsr::get_mmio_address(), dhcsr.into())?;
+        probe.flush()?;
+
+        // Clear VECTOR CATCH and set TRCENA
+        let mut demcr: Demcr = probe.read_word_32(Demcr::get_mmio_address())?.into();
+        demcr.set_trcena(true);
+        demcr.set_vc_corereset(false);
+        probe.write_word_32(Demcr::get_mmio_address(), demcr.into())?;
+        probe.flush()?;
+
+        // Set watch point at SYSTEM_STICK_CALIB access
+        probe.write_word_32(Self::DWT_COMP0, Self::SYSTEM_STICK_CALIB_ADDR)?;
+        probe.write_word_32(Self::DWT_FUNCTION0, 0x00000814)?;
+        probe.flush()?;
+        tracing::debug!("set data watchpoint for MIMXRT5xxS reset");
+
+        // Execute SYSRESETREQ via AIRCR
+        let mut aircr = Aircr(0);
+        aircr.set_sysresetreq(true);
+        aircr.vectkey();
+        // (we need to ignore errors here because the reset will make this
+        // operation seem to have failed.)
+        probe
+            .write_word_32(Aircr::get_mmio_address(), aircr.into())
+            .ok();
+        probe.flush().ok();
+
+        self.wait_for_stop_after_reset(probe)
+    }
+
+    // "ResetHardware" intentionally omitted because the default implementation
+    // seems equivalent to the one in the CMSIS-Pack.
+
+    fn trace_start(
+        &self,
+        interface: &mut dyn ArmProbeInterface,
+        components: &[CoresightComponent],
+        sink: &TraceSink,
+    ) -> Result<(), ArmError> {
+        let mut memory = interface.memory_interface(components[0].ap)?;
+
+        if matches!(sink, TraceSink::Swo(_)) {
+            // The CMSIS-Pack implementation allows choosing between PIO2_24
+            // and PIO2_31 as the SWO pin, using a "debugvar". There's no
+            // equivalent concept here so we'll just force using PIO2_24,
+            // which is the default in the CMSIS-Pack implementation.
+
+            // // Configure PIO2_24: FUNC - 1
+            memory.write_word_32(0x40004160, 0x00000001)?;
+            memory.flush()?;
+        }
+        todo!()
     }
 }
